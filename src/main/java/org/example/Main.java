195f1c14a576381e9e85c2ccefc90eb228423d3e
Sindhu R <sindhu.r0303@gmail.com>
2025-04-23 11:00:38 +0530

    First commit after adding files to docs

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("DevOps MVIT Exp 3");
+        System.out.println("Gradle Project");
+    }
+}
